f970e0b39c2c82a5cb78782591d1e6e98eeeea84
Pavel-Voronov <pavel@asimovcode.com>
2019-04-12 22:15:29 +0300

    change message model

--- src/models/message.js
+++ src/models/message.js
@@ -1,22 +1,22 @@
 module.exports = (sequelize, DataTypes) => {
-    const Message = sequelize.define('message', {
-      author: {
-        allowNull: false,
-        type: DataTypes.STRING
-      },
-      text: {
-        allowNull: false,
-        type: DataTypes.STRING
-      },
-      type: {
-        allowNull: false,
-        type: DataTypes.STRING
-      }
-    }, {});
+	const Message = sequelize.define('message', {
+		author: {
+			allowNull: false,
+			type: DataTypes.STRING
+		},
+		text: {
+			allowNull: false,
+			type: DataTypes.TEXT
+		},
+		type: {
+			allowNull: false,
+			type: DataTypes.STRING
+		}
+	}, {});
 
-    Message.associate = models => {
-        Message.belongsTo(models.Broadcast);
-    };    
-  
-    return Message;
+	Message.associate = models => {
+		Message.belongsTo(models.Broadcast);
+	};    
+
+	return Message;
 };
